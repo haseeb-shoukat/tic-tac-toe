b5247719bfcb130f6d199c0d1c85a729761a51ad
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-07-08 23:11:53 +0500

    Initialize startup files for project

---
+++ script.js
@@ -0,0 +1,19 @@
+const gameBoard = (function() {
+    const boardContents = [];
+    const addToBoard = function(index) {
+
+    };
+
+})();
+
+const displayController = (function() {
+    const displayBoard = document.querySelector("#gameBoard")
+    const render = function() {
+
+    };
+
+})();
+
+const Player = function() {
+
+};

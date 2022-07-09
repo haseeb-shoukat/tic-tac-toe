9e777b564e694f20fdd13f0a05787002aa5e1a19
haseeb-shoukat <haseebshoukat52@gmail.com>
2022-07-09 05:05:27 +0500

    Add logic to update board array
    Tucked away everything in gameLogic module. Made minor changes. Allowed
    ability to enter name for each player. Completed player and board logic

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
-const gameBoard = (function() {
-    const boardContents = [];
-    const addToBoard = function(index) {
+const gameLogic = (function() {
 
-    };
-
-})();
-
-const displayController = (function() {
     let currentState = 1;
     const mainContainer = document.querySelector(".main-container")
 
-    const stateOne = `
-    <div class="game-info">
-        <select class="gamer-one">
-            <option value="p1">Player 1</option>
-        </select>
-        <div class="versus">Vs.</div>
-        <select class="gamer-two">
-            <option value="p2">Player 2</option>
-        </select>
-    </div>
-    <button class="play-btn">Play!</button>`
+    const gameBoard = (function() {
+        const boardContents = ["","","","","","","","",""];
+        const addToBoard = function(index, avatar) {
+            boardContents.splice(index, 1, avatar);
+        };
 
-    const stateTwo = `
-    <div id="0" class="box"></div>
-    <div id="1" class="box"></div>
-    <div id="2" class="box"></div>
-    <div id="3" class="box"></div>
-    <div id="4" class="box"></div>
-    <div id="5" class="box"></div>
-    <div id="6" class="box"></div>
-    <div id="7" class="box"></div>
-    <div id="8" class="box"></div>`
-
-    const render = function() {
-        mainContainer.innerHTML = "";
-        if (currentState === 1) {
-            mainContainer.innerHTML = stateOne;
-                let playBtn = document.querySelector(".play-btn");
-                playBtn.addEventListener("click", changeState);
-        }
-        else if (currentState === 2) {
-            mainContainer.innerHTML = stateTwo;
+        return {boardContents, addToBoard}
+    })();
+    
+    const Player = function(name, role) {
+        let avatar;
+        if (role === "p1") {
+            avatar = "X";
         }
         else {
-
+            avatar = "O";
         }
+
+        return {name, avatar}
     };
+
+    const runGame = (p1, p2) => {
+        const playerOne = Player(p1, "p1");
+        const playerTwo = Player(p2, "p2");
+        const boxes = document.querySelectorAll(".box"); 
+        const boardContents = gameBoard.boardContents
+        let currentTurn = playerOne;
+
+        boxes.forEach(box => {
+            box.addEventListener("click", () => {
+                if (boardContents[box.id] === "") {
+                    
+                    gameBoard.addToBoard(box.id, currentTurn.avatar);
+                    console.log(boardContents)
+
+                    if (currentTurn === playerOne) {
+                        currentTurn = playerTwo;
+                    }
+                    else {
+                        currentTurn = playerOne;
+                    }
+                }
+            })
+        })
+        
+    }
 
     const changeState = () => {
         if (currentState === 1) {
@@ -59,16 +61,57 @@ const displayController = (function() {
             currentState = 1;
         }
 
-        render();
+        displayController.render();
 
     };
 
-    return {render};
+    const displayController = (() => {
+        const stateOne = `
+        <form id="info" class="game-info">
+            <input type="text" class="gamer-one" value="Player 1" required>
+            <div class="versus">Vs.</div>
+            <input type="text" class="gamer-two" value="Player 2" required>
+        </form>
+        <button type="submit" form="info" class="play-btn">Play!</button>`
 
+        const stateTwo = `
+        <div id="0" class="box"></div>
+        <div id="1" class="box"></div>
+        <div id="2" class="box"></div>
+        <div id="3" class="box"></div>
+        <div id="4" class="box"></div>
+        <div id="5" class="box"></div>
+        <div id="6" class="box"></div>
+        <div id="7" class="box"></div>
+        <div id="8" class="box"></div>`
+
+        const render = function() {
+            if (currentState === 1) {
+                mainContainer.innerHTML = "";
+                mainContainer.innerHTML = stateOne;
+                let form = document.querySelector("#info");
+                form.addEventListener("submit", event => {
+                    event.preventDefault();
+                    changeState();
+                });
+            }
+            else if (currentState === 2) {
+                let p1 = document.querySelector(".gamer-one");
+                let p2 = document.querySelector(".gamer-two");
+                mainContainer.innerHTML = "";
+                mainContainer.innerHTML = stateTwo;
+                runGame(p1.value, p2.value);
+            }
+            else {
+
+            }
+        };
+
+        return {render}
+    })();
+
+    return {displayController}
+        
 })();
 
-const Player = function() {
-
-};
-
-displayController.render()
+gameLogic.displayController.render()
